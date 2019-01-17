80b031bc21e0068a65e9d8eef5ae9918ee4864a3
Brent Schneeman <schneeman@gmail.com>
2019-01-17 12:12:12 -0600

    renamed

---
+++ train_model.R
@@ -0,0 +1,50 @@
+library(keras)
+library(tidyverse)
+library(tokenizers)
+library(lubridate)
+
+devtools::load_all()
+
+max_length <- 40
+
+# text <- austen_books() %>%
+#   filter(book == "Pride & Prejudice") %>%
+#   pull(text) %>%
+#   str_c(collapse = " ") %>%
+#   tokenize_characters(lowercase = FALSE, strip_non_alphanum = FALSE, simplify = TRUE)
+
+trump_df <- read_csv("./data/trump_df.csv")
+
+
+text <- trump_df %>% 
+  filter(!is_retweet) %>% 
+  arrange(created_at) %>%
+  top_n(3300, created_at) %>%
+  ## whack the emojis
+  clean_and_tokenize()
+
+
+print(sprintf("Corpus length: %d", length(text)))
+
+alphabet <- text %>%
+  unique() %>%
+  sort()
+
+print(sprintf("Total characters: %d", length(alphabet)))
+
+
+vectors <- get_vectors(text, alphabet, max_length)
+
+model <- create_model(alphabet, max_length)
+
+model_history <- fit_model(model, vectors, epochs = 1)
+
+model %>% save_model_hdf5("./trumprnn.h5")
+
+model <- load_model_hdf5("./trumprnn.h5")
+
+generate_phrase(model, trump_df %>% 
+                  top_n(n = 2, wt = created_at) %>% 
+                  clean_and_tokenize(), alphabet, max_length, 230, 0.5)
+
+plot(model_history)
