31b0c08f69ce689e3cc18e8942d16f6af4db30d5
Brent Schneeman <schneeman@gmail.com>
2019-01-19 13:05:16 -0600

    looking at early stopping

--- train_model.R
+++ train_model.R
@@ -2,6 +2,7 @@ library(keras)
 library(tidyverse)
 library(tokenizers)
 library(lubridate)
+library(ggplot2)
 
 devtools::load_all()
 
@@ -30,16 +31,15 @@ vectors <- get_vectors(text, alphabet, max_length)
 
 model <- create_model(alphabet, max_length)
 
-model_history <- fit_model(model, vectors, epochs = 40, view_metrics = TRUE)
+model_history <- fit_model(model, vectors, epochs = 40, view_metrics = FALSE)
+
+saveRDS(model_history, "model_history.RDS")
 
 model %>% save_model_hdf5("./trumprnn.h5")
 
 model <- load_model_hdf5("./trumprnn.h5")
 
 generate_phrase(model, trump_df %>% 
-                  top_n(n = 2, wt = created_at) %>% 
+                  top_n(n = 5, wt = created_at) %>% 
                   clean_and_tokenize(), alphabet, max_length, 230, 0.5)
 
-plot(model_history)
-
-saveRDS(model_history, "model_history.RDS")
